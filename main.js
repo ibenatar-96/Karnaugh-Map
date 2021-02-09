2a9a9067d1de22895f9ce3d5939230c51fe3eaac
iBenatar-96 <iBenatar-96@users.noreply.github.com>
2021-02-09 11:37:45 +0200

    change

--- main.js
+++ main.js
@@ -215,6 +215,7 @@ function areSame(value1,value2){
         return false;
 }
 function mergeSame(same){
+    debugger; //TODELETE!!
     for(var i=0; i<same.length; i++){
         var j = i+1;
         while(j<same.length){
@@ -223,11 +224,14 @@ function mergeSame(same){
                 same[i].mul = 2*same[i].mul;
                 same.splice(j,1);
             }
+            else{
+                j++;
+            }
         }
     }
 }
 function areMergeable(value1,value2){
-    if(value1.row + value2.row + value1.i == rowSize &&
+    if(value1.row + value2.row + value1.i == rowSize && value1.col + value2.col + value1.j == colSize &&
         value1.i == value2.i && value1.j == value2.j && value1.mul == value2.mul &&
         value1.symcol == value2.symcol && value1.symrow == value2.symrow){
             return true;
@@ -262,7 +266,6 @@ function removeOverlap(){
     var f = 0;
     var changedCol = false;
     var changedRow = false;
-    debugger;
     while(f<same.length){
         for(var h=0; h<same[f].i; h++){
             for(var g=0; g<same[f].j; g++){
@@ -292,17 +295,18 @@ function removeOverlap(){
             same[f].symcol = false;
             changedCol = true;
         }
-        if(changedCol==true && changedRow==true){
+        else if(changedCol==true & changedRow==true & same[f].symcol==false && same[f].symrow==false){
             same[f].col = colSize-same[f].col-same[f].j;
-            continue; //THIS DOES NOT WORK!! CONTINUE SUCKSSS
-    }
-        else if(same[f].symcol==false && same[f].symrow==false){
-            f++;
             changedCol = changedRow = false;
+        }
+        else if(same[f].symcol==false && same[f].symrow==false){
+            changedCol = changedRow = false;
+            f++;
         }
 }
     var k=0;
     while(k<arr.length){
+        debugger;
         var x = true;
         for(var h=0; x==true && h<arr[k].i; h++){
             for(var g=0; x==true && g<arr[k].j; g++){
@@ -312,10 +316,15 @@ function removeOverlap(){
                 }
                 overlap[arr[k].row+h][arr[k].col+g] = overlap[arr[k].row+h][arr[k].col+g] + 1;
                 if(x==true && h==arr[k].i-1 && g==arr[k].j-1){
+//                    for(var a=0; x==true && a<arr[k].i; a++){ //IS THIS NECCASERY?!?!?!?
+//                        for(var b=0; x==true && b<arr[k].j; b++){ //IS THIS NECCASERY!?!
+//                            overlap[arr[k].row+a][arr[k].col+b] = overlap[arr[k].row+a][arr[k].col+b] - 1; //?!?!
                     arr.splice(k,1);
-                }
-            }
+//                }
+//            }
         }
+    }
+}
         if(x==false){
             k++;
         }
